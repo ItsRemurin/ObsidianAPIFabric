3031ed91415c26ca60c883117a3d7f79decfb13d
ItsRemurin <ItsRemurin@gmail.com>
2023-10-11 09:22:45 -0400

    Added Vanilla LandBiomes

--- src/main/java/com/obsidian/ObsidianMod.java
+++ src/main/java/com/obsidian/ObsidianMod.java
@@ -3,25 +3,20 @@ package com.obsidian;
 import com.obsidian.endbiomes.TheEndBiomeData;
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.fabricmc.fabric.api.event.registry.DynamicRegistrySetupCallback;
-import net.fabricmc.fabric.mixin.biome.TheEndBiomeSourceMixin;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
-import net.minecraft.world.biome.source.MultiNoiseBiomeSource;
-import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class ObsidianMod implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("obsidian");
 	@Override
 	public void onInitialize() {
-		TheEndBiomeData.AddLandBiome(BiomeKeys.END_HIGHLANDS);
+		TheEndBiomeData.addLandBiome(BiomeKeys.END_HIGHLANDS);
+		TheEndBiomeData.addLandBiome(BiomeKeys.END_MIDLANDS);
+		TheEndBiomeData.addLandBiome(BiomeKeys.END_BARRENS);
+
+		//DimensionOptions.END.getValue().
+
+		//MaterialRuleData.AddEndBiomeMaterialRule(BiomeKeys.END_MIDLANDS, MaterialRules.condition(MaterialRules.biome(BiomeKeys.THE_END), MaterialRules.block(Blocks.GRASS_BLOCK.getDefaultState())));
 	}
 }
