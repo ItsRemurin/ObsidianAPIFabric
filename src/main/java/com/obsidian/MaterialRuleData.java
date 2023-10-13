40457acbfec0ae1ed9666853c4bb690f44557039
ItsRemurin <ItsRemurin@gmail.com>
2023-10-13 08:28:46 -0400

    Added Multi-Dimension support for MaterialRuleData

---
+++ src/main/java/com/obsidian/MaterialRuleData.java
@@ -0,0 +1,47 @@
+package com.obsidian;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import com.mojang.authlib.minecraft.client.MinecraftClient;
+import com.obsidian.ObsidianMod;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.dimension.DimensionTypes;
+import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
+import net.minecraft.world.gen.surfacebuilder.MaterialRules;
+import net.minecraft.world.gen.surfacebuilder.VanillaSurfaceRules;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class MaterialRuleData {
+    private static final BiMap<RegistryKey<DimensionType>, Set<MaterialRules.MaterialRule>> materialRulesMap = HashBiMap.create();
+
+    public static Set<MaterialRules.MaterialRule> getDimensionMaterialRules(RegistryKey<DimensionType> dimensionType) {
+        return materialRulesMap.get(dimensionType);
+    }
+
+
+    public static void addEndMaterialRule(MaterialRules.MaterialRule materialRule) {
+        addMaterialRule(DimensionTypes.THE_END, materialRule);
+    }
+    public static void addNetherMaterialRule(MaterialRules.MaterialRule materialRule) {
+        addMaterialRule(DimensionTypes.THE_NETHER, materialRule);
+    }
+    public static void addOverworldMaterialRule(MaterialRules.MaterialRule materialRule) {
+        addMaterialRule(DimensionTypes.OVERWORLD, materialRule);
+    }
+    public static void addMaterialRule(RegistryKey<DimensionType> dimensionType, MaterialRules.MaterialRule materialRule) {
+        if(!materialRulesMap.containsKey(dimensionType)) {
+            materialRulesMap.put(dimensionType, new HashSet<>());
+        }
+        materialRulesMap.get(dimensionType).add(materialRule);
+    }
+}
--- src/main/java/com/obsidian/mixin/ChunkGeneratorSettingsMixin.java
+++ src/main/java/com/obsidian/mixin/ChunkGeneratorSettingsMixin.java
@@ -1,24 +1,43 @@
 package com.obsidian.mixin;
 
-import com.obsidian.endbiomes.MaterialRuleData;
+import com.mojang.serialization.Codec;
+import com.obsidian.MaterialRuleData;
+import net.minecraft.world.dimension.DimensionTypes;
 import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
+import net.minecraft.world.gen.chunk.GenerationShapeConfig;
 import net.minecraft.world.gen.surfacebuilder.MaterialRules;
-import net.minecraft.world.gen.surfacebuilder.VanillaSurfaceRules;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Set;
+
 @Mixin(ChunkGeneratorSettings.class)
 public class ChunkGeneratorSettingsMixin {
 
+    @Shadow @Final public static Codec<ChunkGeneratorSettings> CODEC;
+
+
+    @Shadow @Final private GenerationShapeConfig generationShapeConfig;
+
     @Inject(method = "surfaceRule", at = @At("RETURN"), cancellable = true)
     public void surfaceRule(CallbackInfoReturnable<MaterialRules.MaterialRule> cir) {
-        if(cir.getReturnValue().equals(VanillaSurfaceRules.getEndStoneRule())) {
-            cir.setReturnValue(
-                    MaterialRules.sequence(MaterialRules.sequence(MaterialRuleData.endBiomeMaterialRules.values().toArray(MaterialRules.MaterialRule[]::new)),
-                            VanillaSurfaceRules.getEndStoneRule())
-            );
+        Set<MaterialRules.MaterialRule> newRuleSet = null;
+
+        if (generationShapeConfig == GenerationShapeConfig.END) {
+            newRuleSet = MaterialRuleData.getDimensionMaterialRules(DimensionTypes.THE_END);
+        } else if (generationShapeConfig == GenerationShapeConfig.NETHER) {
+            newRuleSet = MaterialRuleData.getDimensionMaterialRules(DimensionTypes.THE_NETHER);
+        } else if (generationShapeConfig == GenerationShapeConfig.SURFACE) {
+            newRuleSet = MaterialRuleData.getDimensionMaterialRules(DimensionTypes.OVERWORLD);
+        }
+
+        if (newRuleSet != null) {
+            MaterialRules.MaterialRule oldReturnValue = cir.getReturnValue();
+            cir.setReturnValue(MaterialRules.sequence(MaterialRules.sequence(newRuleSet.toArray(MaterialRules.MaterialRule[]::new)), oldReturnValue));
         }
     }
 }
