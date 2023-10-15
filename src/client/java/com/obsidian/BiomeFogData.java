af9c43a133c2a391d6804087bbd58547cbf0e4d3
ItsRemurin <ItsRemurin@gmail.com>
2023-10-14 22:42:52 -0400

    Update BiomeFogData.java

--- src/client/java/com/obsidian/BiomeFogData.java
+++ src/client/java/com/obsidian/BiomeFogData.java
@@ -6,18 +6,24 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.world.biome.Biome;
 
 public class BiomeFogData {
-    public static final BiMap<RegistryKey<Biome>, FogModificationData> biomeFogDataMap = HashBiMap.create();
+    private static final BiMap<RegistryKey<Biome>, FogModificationData> biomeFogDataMap = HashBiMap.create();
 
     public static FogModificationData getBiomeFogData(RegistryKey<Biome> biomeRegistryKey) {
         return biomeFogDataMap.get(biomeRegistryKey);
     }
+    private static boolean setBiomeFogData(RegistryKey<Biome> biomeRegistryKey, FogModificationData data) {
+        if(biomeFogDataMap.containsKey(biomeRegistryKey)) {
+            return false;
+        }
 
-    public static void setBiomeFogData(RegistryKey<Biome> biomeRegistryKey, FogModificationData data) {
         biomeFogDataMap.put(biomeRegistryKey, data);
+        return true;
     }
-
-    public static void setBiomeFogData(RegistryKey<Biome> biomeRegistryKey, float fogStartMult, float fogEndMult) {
-        biomeFogDataMap.put(biomeRegistryKey,  new FogModificationData(fogStartMult, fogEndMult));
+    public static boolean setBiomeFogData(RegistryKey<Biome> biomeRegistryKey, float fogStartMult, float fogEndMult) {
+        return setBiomeFogData(biomeRegistryKey, new FogModificationData(fogStartMult, fogEndMult));
+    }
+    public static boolean setBiomeFogData(RegistryKey<Biome> biomeRegistryKey, float fogDensityMult) {
+        return setBiomeFogData(biomeRegistryKey, fogDensityMult, fogDensityMult);
     }
 
     public record FogModificationData(float fogStartMult, float fogEndMult) { }
