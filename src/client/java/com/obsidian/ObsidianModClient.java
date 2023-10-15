b2796511233c2ebe7dadf5c4ca0369db51afbba4
ItsRemurin <ItsRemurin@gmail.com>
2023-10-14 21:34:17 -0400

    Removed test code

--- src/client/java/com/obsidian/ObsidianModClient.java
+++ src/client/java/com/obsidian/ObsidianModClient.java
@@ -6,7 +6,5 @@ import net.minecraft.world.biome.BiomeKeys;
 public class ObsidianModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-		BiomeFogData.setBiomeFogData(BiomeKeys.PLAINS, new BiomeFogData.FogModificationData(6, 2));
 	}
 }
